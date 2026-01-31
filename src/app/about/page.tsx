6474e7e3455c26dcca917d13137838121bc73ac5
burhan11231 <burhan.ah.shkh@gmail.com>
2026-01-31 21:51:55 +0530

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'About TECHMATE',
     description:
-      'Learn about TECHMATE, a trusted laptop showroom in Srinagar, Kashmir since 2020.',
+      'Learn about TECHMATE, a trusted laptop showroom in Srinagar, Kashmir.',
     url: 'https://techmate-srinagar.netlify.app/about',
     siteName: 'TECHMATE Gadgets',
     type: 'website',
