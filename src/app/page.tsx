f3aa407bb51a4bcf6a7654500465c4bc2eafbeed
burhan11231 <burhan.ah.shkh@gmail.com>
2026-01-31 20:31:57 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,41 +3,41 @@ import type { Metadata } from 'next'
 import HomeClient from './HomeClient'
 
 const OG_IMAGE =
-  'https://res.cloudinary.com/dlesei0kn/image/upload/mitc-home-og-image_xeqsjx.jpg'
+  'https://res.cloudinary.com/dlesei0kn/image/upload/v1769869958/Techmate-white_gzqunv.jpg'
 
 export const metadata: Metadata = {
-  title: 'MITC – Laptop Sales & IT Services in Srinagar',
+  title: 'Laptop Sales & IT Services in Srinagar',
   description:
     'Expert guidance and in-store diagnostics, handled personally. Explore our services and contact the MITC team directly through our website.',
 
   alternates: {
-    canonical: 'https://mitc-business-platform.netlify.app/',
+    canonical: 'https://techmate-srinagar.netlify.app/',
   },
 
   openGraph: {
     type: 'website',
     locale: 'en_IN',
-    siteName: 'MITC – Mateen IT Corp',
+    siteName: 'TECHMATE',
 
     title: 'MITC – Laptop Sales & IT Services in Srinagar',
     description:
-      'Expert guidance and in-store diagnostics, handled personally. Explore our services and contact the MITC team directly through our website.',
+      'Expert guidance and in-store diagnostics, handled personally. Explore our services and contact the team directly through our website.',
 
     images: [
       {
         url: OG_IMAGE,
         width: 1200,
         height: 630,
-        alt: 'MITC – Laptop Sales & IT Services in Srinagar',
+        alt: 'Laptop Sales & IT Services in Srinagar',
       },
     ],
   },
 
   twitter: {
     card: 'summary_large_image',
-    title: 'MITC – Laptop Sales & IT Services in Srinagar',
+    title: 'Laptop Sales & IT Services in Srinagar',
     description:
-      'Expert guidance and in-store diagnostics, handled personally. Explore our services and contact the MITC team directly through our website.',
+      'Expert guidance and in-store diagnostics, handled personally. Explore our services and contact the team directly through our website.',
     images: [OG_IMAGE],
   },
 }
