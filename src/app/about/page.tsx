00f7ed9161c97e5e651fc99639e03e0fddac00ad
burhan11231 <burhan.ah.shkh@gmail.com>
2026-01-31 20:51:55 +0530

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,27 +5,27 @@ import AboutClient from './AboutClient';
 import LocalBusinessSchema from './LocalBusinessSchema';
 
 export const metadata: Metadata = {
-  title: 'About MITC | Trusted Laptop Showroom in Srinagar, Kashmir',
+  title: 'About TECHMATE | Trusted Laptop Showroom in Srinagar, Kashmir',
   description:
-    'Learn about MITC (Mateen IT Corp), a trusted laptop showroom in Srinagar, Kashmir since 2013.',
+    'Learn about TECHMATE, a trusted laptop showroom in Srinagar, Kashmir since 2020.',
 
   alternates: {
-    canonical: 'https://mitck.netlify.app/about',
+    canonical: 'https://techmate-srinagar.netlify.app/about',
   },
 
   openGraph: {
-    title: 'About MITC – Mateen IT Corp',
+    title: 'About TECHMATE',
     description:
       'A trusted laptop showroom in Srinagar offering honest guidance since 2013.',
-    url: 'https://mitck.netlify.app/about',
-    siteName: 'MITC – Mateen IT Corp',
+    url: 'https://techmate-srinagar.netlify.app/about',
+    siteName: 'TECHMATE Gadgets',
     type: 'website',
     images: [
       {
-        url: 'https://res.cloudinary.com/dlesei0kn/image/upload/IMG-20251103-WA0003_bgmgkj.jpg',
+        url: 'https://res.cloudinary.com/dlesei0kn/image/upload/v1769869958/Techmate-white_gzqunv.jpg',
         width: 1200,
         height: 630,
-        alt: 'MITC Laptop Showroom in Srinagar',
+        alt: 'TECHMATE Laptop Showroom in Srinagar',
       },
     ],
   },
