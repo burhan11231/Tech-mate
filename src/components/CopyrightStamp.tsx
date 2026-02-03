4f5ffdc9218c0038b6e60410beeb3a1c8ea8a16a
burhan11231 <burhan.ah.shkh@gmail.com>
2026-02-03 22:41:31 +0530

    Update CopyrightStamp.tsx

--- src/components/CopyrightStamp.tsx
+++ src/components/CopyrightStamp.tsx
@@ -1,15 +1,17 @@
 export default function CopyrightStamp() {
   return (
-    <div
-      className="fixed inset-0 z-[9999] pointer-events-none flex items-center justify-center"
-      style={{ opacity: 0.35 }}
-    >
+    <div className="fixed inset-0 z-[9999] pointer-events-none flex items-center justify-center">
       <img
         src="/stamps/kashpages-copyright.svg"
-        alt="KashPages Preview Copyright"
-        className="w-[360px] rotate-[-15deg]"
+        alt=""
+        className="
+          w-[520px]
+          max-w-[80vw]
+          rotate-[-14deg]
+          opacity-[0.18]
+          select-none
+        "
       />
     </div>
   )
 }
-
