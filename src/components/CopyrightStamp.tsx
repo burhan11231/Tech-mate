be974c547ac17f7cd26d61dd66b75950f4a16df2
burhan11231 <burhan.ah.shkh@gmail.com>
2026-02-03 22:47:26 +0530

    Increase max width of copyright stamp image
    Updated the maximum width of the copyright stamp image from 80vw to 85vw for better visibility.

--- src/components/CopyrightStamp.tsx
+++ src/components/CopyrightStamp.tsx
@@ -1,12 +1,25 @@
 export default function CopyrightStamp() {
   return (
-    <div className="fixed inset-0 z-[9999] pointer-events-none flex items-center justify-center">
+    <div
+      className="
+        fixed
+        top-0 left-0
+        w-screen h-screen
+        pointer-events-none
+      "
+      style={{
+        zIndex: 2147483647, // max safe z-index
+      }}
+    >
       <img
         src="/stamps/kashpages-copyright.svg"
         alt=""
         className="
+          absolute
+          top-1/2 left-1/2
+          -translate-x-1/2 -translate-y-1/2
           w-[520px]
-          max-w-[80vw]
+          max-w-[85vw]
           rotate-[-14deg]
           opacity-[0.18]
           select-none
